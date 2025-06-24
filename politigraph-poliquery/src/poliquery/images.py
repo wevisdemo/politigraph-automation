189a372d566d959a2ccd74a9f96b176706a8bea1
patiphan2000 <srichai.patiphan@gmail.com>
2025-06-24 20:57:10 +0700

    feat(poliquery): add update party logo url function

--- politigraph-poliquery/src/poliquery/images.py
+++ politigraph-poliquery/src/poliquery/images.py
@@ -1,6 +1,8 @@
 from gql import Client
 
 from .query_helper.persons import update_image_url
+from .query_helper.organizations import update_organiztion
+
 def update_politician_image_url(
     client: Client,
     firstname: str,
@@ -19,3 +21,19 @@ def update_politician_image_url(
     result = update_image_url(client=client, params=params)
     return result["updatePeople"]["people"][0] if result["updatePeople"]["people"] else None
     
+def update_party_logo_image_url(
+    client: Client,
+    party_name: str,
+    image_url: str
+) -> dict:
+    params = {
+        "where": {
+            "classification_EQ": "POLITICAL_PARTY",
+            "name_EQ": party_name
+        },
+        "update": {
+            "image_SET": image_url
+        }
+    }
+    result = update_organiztion(client=client, params=params)
+    return result
