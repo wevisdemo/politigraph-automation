11d98242dbe3a5fbf40bdf7d2b7284d0c3495a90
patiphan2000 <srichai.patiphan@gmail.com>
2025-09-23 11:08:26 +0700

    feat(bills-scraper): add extract_vote_count to utility

--- politigraph-bills-scraper/src/lis_bills_scraper/utility.py
+++ politigraph-bills-scraper/src/lis_bills_scraper/utility.py
@@ -19,3 +19,43 @@ def convert_thai_date_to_universal(thai_date_str:str) -> str:
 
     # Format the new datetime object into the desired yyyy-mm-dd string format
     return universal_dt_object.strftime('%Y-%m-%d')
+
+def extract_vote_count_data(
+    vote_text: str,
+    vote_option_index: Dict[str, str]|None= {
+        'agree_count': 'เห็นชอบ',
+        'disagree_count': 'ไม่เห็นชอบ',
+        'abstain_count': 'งดออกเสียง',
+        'novote_count': 'ไม่ลงคะแนน',
+    }
+) -> Dict[str, Any]:
+    
+    if not vote_option_index:
+        raise ValueError("Invalid vote option index!!")
+    
+    # Get & Normalize vote text
+    vote_text = vote_text.replace("\r", "")
+    # vote_text = vote_text.replace("\n", " | ")
+    vote_text = re.sub(r"\s+", " ", vote_text)
+    vote_text = convert_thai_number_str_to_arabic(vote_text) # convert Thai num to Arabic
+    # print(vote_text)
+
+    vote_count_data = {}
+    for option, option_text in vote_option_index.items():
+        if option_text not in vote_text:
+            vote_count_data[option] = None
+        _pattern = r"^(" + option_text + r").+?(เสียง|ไม่มี)"
+        match_option = re.search(_pattern, vote_text)
+        if match_option:
+            _txt = match_option.group(0)
+            match_num = re.search(r"\d+", _txt)
+            if "ไม่มี" in _txt:
+                vote_count_data[option] = 0
+            elif match_num:
+                vote_count_data[option] = int(match_num.group(0))
+            vote_text = re.sub(_pattern, "", vote_text).strip()
+    # If novote_count not present set to 0
+    if not vote_count_data.get('novote_count', None):
+        vote_count_data['novote_count'] = 0
+    
+    return vote_count_data
