2589b3eaec557254c22de23703ee18efe41fff57
patiphan2000 <srichai.patiphan@gmail.com>
2025-09-23 11:08:55 +0700

    fix(bills-scraper): add necessary import to utility

--- politigraph-bills-scraper/src/lis_bills_scraper/utility.py
+++ politigraph-bills-scraper/src/lis_bills_scraper/utility.py
@@ -1,4 +1,8 @@
+from typing import Dict, Any
+import re
 from datetime import datetime
+
+from thai_name_normalizer import convert_thai_number_str_to_arabic
 
 def convert_thai_date_to_universal(thai_date_str:str) -> str:
     
