e6d9082434c601fed74d8722ba66b2a7f60b15ad
patiphan2000 <srichai.patiphan@gmail.com>
2025-09-29 11:23:00 +0700

    feat(poliquery): add functionality for BillMergeEvent

--- politigraph-poliquery/src/poliquery/bill_events_handler.py
+++ politigraph-poliquery/src/poliquery/bill_events_handler.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Any, Hashable
 import asyncio
+from gql import Client
 
 from .apollo_connector import get_apollo_client
 from .query_helper.vote_events import agg_count_vote_events
@@ -7,6 +8,8 @@ from .query_helper.royal_assent_event import get_royal_assent_events, create_roy
 from .query_helper.enforce_event import agg_count_enforce_event, create_enforce_event
 from .query_helper.reject_event import agg_count_reject_event, create_reject_event
 from .query_helper.draft_vote_event import agg_count_draft_vote_events, update_draft_vote_event, create_draft_vote_event
+from .query_helper.bills import get_bills
+from .query_helper.merge_event import agg_count_merge_event, create_merge_event, update_merge_event
 
 def is_vote_event_exist_in_bill(
     bill_id: str,
@@ -344,3 +347,95 @@ def create_new_reject_event(
         client=apollo_client,
         params=params
     ))
+    
+async def create_and_connect_merge_event(
+    client: Client,
+    merged_bill_ids: List[str]
+):
+    # Create new merge event
+    create_param = {
+        "input": [{"publish_status": "UNPUBLISHED"}]
+    }
+    result = await create_merge_event(
+        client=client,
+        params=create_param
+    )
+    
+    new_mereg_event_id = result['createBillMergeEvents']['billMergeEvents'][0]['id']
+    
+    # Connect merge event to bills
+    for bill_id in merged_bill_ids:
+        update_param = {
+            "where": {
+                "id_EQ": new_mereg_event_id
+            },
+            "update": {
+                "motions": [
+                    {
+                        "connect": [{
+                            "where": {
+                                "node": {
+                                    "id_EQ": bill_id
+                                }
+                            }
+                        }]
+                    }
+                ]
+            }
+        }
+        await update_merge_event(
+            client=client,
+            params=update_param
+        )
+
+def create_new_merge_event(
+    bill_info: Dict[str, Any],
+    event_info: Dict[str, Any]
+) -> None:
+    
+    # Initiate client
+    apollo_client = get_apollo_client()
+    
+    # Get bill ID
+    bill_id = bill_info.get('id', "")
+    
+    # Get merged bills data
+    merged_bills_data = event_info.get('merged_bills', [])
+    
+    # Get acceptance numbers & lis_ids to query all merged bills
+    merged_acceptance_numbers = [d['acceptance_number'] for d in merged_bills_data]
+    merged_lis_ids = [d['lis_id'] for d in merged_bills_data]
+    
+    # Query all bill involve to get ID
+    merged_bills = asyncio.run(get_bills(
+        client=apollo_client,
+        fields=['id', 'acceptance_number', 'lis_id'],
+        params={
+            "where": {
+                "acceptance_number_IN": merged_acceptance_numbers,
+                "lis_id_IN": merged_lis_ids
+            }
+        }
+    ))
+    
+    # Use bill's ID to query merge event
+    merged_bill_ids = [d['id'] for d in merged_bills]
+    if asyncio.run(agg_count_merge_event(
+        client=apollo_client,
+        params={
+            "where": {
+                "motions_SOME": {
+                    "id_IN": merged_bill_ids
+                }
+            }
+        }
+    )):
+        print("found merge event")
+        return # skip
+    
+    # Create new merge event
+    asyncio.run(create_and_connect_merge_event(
+        client=apollo_client,
+        merged_bill_ids=merged_bill_ids
+    ))
+    
---
+++ politigraph-poliquery/src/poliquery/query_helper/merge_event.py
@@ -0,0 +1,99 @@
+from typing import List, Dict, Any
+from gql import gql
+from gql import Client
+
+from .schema import get_allowed_fields_for_type
+
+async def get_merge_events(client:Client, fields:List[str], params:Dict={}) -> List[Dict[str, Any]]:
+    """
+    Query BillMergeEvent node data with given fields.
+
+    Args:
+        client: gql.Client
+            The GQL client with a fetched schema.
+        fields: list
+            Field of data to query.
+        params: dict, optional
+            Dictionary object of query parameter.
+
+    Returns:
+        A list of BillMergeEvent data.
+    """
+    
+    # Load schema to get valid fields
+    valid_property = await get_allowed_fields_for_type(client=client, type_name='BillMergeEvent')
+    
+    # Check if any fields in the list is invalid
+    import re
+    if any(re.sub(r"\s.*", "", prop) not in valid_property for prop in fields):
+        raise ValueError("Invalid field name.")
+
+    fields_string = "\n            ".join(fields)
+    query_string = f"""
+    query BillMergeEvents($where: BillMergeEventWhere, $sort: [BillMergeEventSort!]) {{
+        billMergeEvents(where: $where, sort: $sort) {{
+            {fields_string}
+        }}
+    }}
+    """
+    query = gql(query_string)
+    async with client as session:
+        result = await session.execute(query, variable_values=params)  
+        return result['billMergeEvents']
+    
+async def create_merge_event(client: Client, params: dict) -> Dict[str, Any]:
+    query = gql(
+    """
+    mutation CreateBillMergeEvents($input: [BillMergeEventCreateInput!]!) {
+        createBillMergeEvents(input: $input) {
+            info {
+                nodesCreated
+                relationshipsCreated
+            }
+            billMergeEvents {
+                id
+            }
+        }
+    }
+    """
+    )
+    async with client as session:
+        result = await session.execute(query, variable_values=params)  
+        return result
+    
+async def update_merge_event(client: Client, params: dict):
+    query = gql(
+    """
+    mutation UpdateBillMergeEvents($where: BillMergeEventWhere, $update: BillMergeEventUpdateInput) {
+        updateBillMergeEvents(where: $where, update: $update) {
+            info {
+            nodesCreated
+            nodesDeleted
+            relationshipsCreated
+            relationshipsDeleted
+            }
+        }
+    }
+    """
+    )
+    async with client as session:
+        result = await session.execute(query, variable_values=params)  
+        return result 
+
+async def agg_count_merge_event(client: Client, params: dict) -> int:
+    query = gql(
+    """
+    query BillMergeEventsConnection {
+        billMergeEventsConnection {
+            aggregate {
+                count {
+                    nodes
+                }
+            }
+        }
+    }
+    """
+    )
+    async with client as session:
+        result = await session.execute(query, variable_values=params)  
+        return result['billMergeEventsConnection']['aggregate']['count']['nodes']
