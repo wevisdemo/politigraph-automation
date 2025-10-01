50f5c022bdadde64eb2bbe2b2cc411ae7d307869
patiphan2000 <srichai.patiphan@gmail.com>
2025-10-01 20:04:11 +0700

    fix(bills-scraper): update convert_thai_date_to_universal

--- politigraph-bills-scraper/src/lis_bills_scraper/utility.py
+++ politigraph-bills-scraper/src/lis_bills_scraper/utility.py
@@ -9,23 +9,26 @@ def convert_thai_date_to_universal(thai_date_str:str|None) -> str|None:
     if not thai_date_str:
         return None
     
-    # Parse the date string in the format dd/mm/yyyy
-    dt_object = datetime.strptime(thai_date_str, '%d/%m/%Y')
+    # Split the input string into day, month, and Thai year
+    day, month, thai_year = thai_date_str.split('/')
+
+    # Convert the Thai year to an integer
+    universal_year = int(thai_year)
     
     # Check whether the year is valid
-    if dt_object.year < 2500: # year if already in Gregorian year
+    if universal_year < 2500: # year if already in Gregorian year
         # TODO update this before the year 2100 or successfully achieve Open Parliament
-        year_2_digit = int(str(dt_object.year)[2:])
+        year_2_digit = int(str(universal_year)[2:])
         gregorian_year = 2000 + year_2_digit
     else:
         # Convert the Thai year to Gregorian year by subtracting 543
-        gregorian_year = dt_object.year - 543
+        gregorian_year = universal_year - 543
 
     # Create a new datetime object with the corrected year
-    universal_dt_object = dt_object.replace(year=gregorian_year)
+    date_obj = datetime(gregorian_year, int(month), int(day))
 
     # Format the new datetime object into the desired yyyy-mm-dd string format
-    return universal_dt_object.strftime('%Y-%m-%d')
+    return date_obj.strftime('%Y-%m-%d')
 
 def extract_vote_num(num_text: str) -> int:
     if 'ไม่มี' in num_text:
