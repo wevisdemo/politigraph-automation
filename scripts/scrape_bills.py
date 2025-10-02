eef00daeda87a233d4498d52f2357b705d1d1f11
patiphan2000 <srichai.patiphan@gmail.com>
2025-10-02 15:39:01 +0700

    feat(scripts): Update scrape_bills script to scrape every parliament terms in politigraph database

--- scripts/scrape_bills.py
+++ scripts/scrape_bills.py
@@ -1,21 +1,33 @@
 # /// script
 # requires-python = "==3.10.11"
 # dependencies = [
-#     "thai_name_normalizer", "poliquery",
+#     "thai_name_normalizer", "poliquery", "msbis-vote-events-scraper",
 #     "lis_bills_scraper"
 # ]
 # [tool.uv.sources]
 # poliquery = { path = "../politigraph-poliquery", editable = true }
+# msbis-vote-events-scraper = { path = "../politigraph_vote_events_scraper", editable = true }
 # lis_bills_scraper = { path = "../politigraph-bills-scraper", editable = true }
 # thai_name_normalizer = { path = "../politigraph-name-normalizer", editable = true }
 # ///
 
-from lis_bills_scraper import scrape_and_create_new_bills
+from lis_bills_scraper import scrape_and_create_new_bills, scrape_and_update_bill_events, cleanup_bill
+from poliquery import get_all_house_of_representatives
 
 def main() -> None:
-    print("Hello from Bills scraper")
+    # Get all House of Representatives
+    hor = get_all_house_of_representatives()
+    terms = [
+        h['term'] for h in hor
+    ]
     
-    scrape_and_create_new_bills(25)
+    # Scrape bill & billEvent for each term
+    for term in terms:
+        print(f"Scrape Bill for House of Representatives {term}th term...")
+        scrape_and_create_new_bills(term)
+        scrape_and_update_bill_events(term)
+        print(f"Cleanup BillEvent for House of Representatives {term}th term...")
+        cleanup_bill(term)
 
 if __name__ == "__main__":
     main()
