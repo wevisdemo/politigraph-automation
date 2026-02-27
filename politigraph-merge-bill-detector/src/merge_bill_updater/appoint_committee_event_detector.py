0005afb8d43cc321e09c1d4fe45fd1b39565827c
patiphan2000 <srichai.patiphan@gmail.com>
2026-02-27 12:54:52 +0700

    feat(bill-scraper): add cert to update merge event

--- politigraph-merge-bill-detector/src/merge_bill_updater/appoint_committee_event_detector.py
+++ politigraph-merge-bill-detector/src/merge_bill_updater/appoint_committee_event_detector.py
@@ -1,11 +1,27 @@
 from typing import List, Dict, Any, Tuple
 
 import re
-import requests, time
+import os, requests
+import tempfile
 from bs4 import BeautifulSoup, Tag
 
 APPOINT_COMMITEE_SECTION_TITLE = "ข้อมูลแต่งตั้งคณะกรรมาธิการสภาผู้แทนราษฎร"
 COMMITEE_RESULT_SECTION_TITLE = "ข้อมูลผลการพิจารณาของคณะกรรมาธิการ"
+
+def create_temp_cert_file():
+    
+    CLIENT_CERT_STRING = os.getenv('CLIENT_CERT_STRING')
+    if not CLIENT_CERT_STRING:
+        raise ValueError("No CLIENT_CERT_STRING")
+    
+    # Create temporary files
+    with tempfile.NamedTemporaryFile(delete=False) as f_cert, \
+        tempfile.NamedTemporaryFile(delete=False) as f_key:
+        f_cert.write(CLIENT_CERT_STRING.encode("utf-8"))
+        f_cert.seek(0)
+        
+        cert_path = f_cert.name
+        return cert_path
 
 def get_attached_doc(section: Tag) -> str|None:
     # Check if any doc is attached to this event
@@ -19,7 +35,9 @@ def get_attached_doc(section: Tag) -> str|None:
         return doc_url # type: ignore
 
 def get_appoint_committee_event_doc(lis_url: str) -> List[str|None]:
-    response = requests.get(lis_url)
+    
+    cert_file_path = create_temp_cert_file()
+    response = requests.get(lis_url, verify=cert_file_path)
     
     soup = BeautifulSoup(BeautifulSoup(response.content, "html.parser").decode(), "html.parser")
     
